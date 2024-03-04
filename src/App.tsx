52fe8f1a4e0cebf5da3efc2c71663c6a6bc92ee3
vertskater <vertskater@gmx.at>
2024-03-04 09:25:39 +0100

    Add player name and time to Game component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import "./App.css";
 import Cursor from "./Cursor";
 import Game from "./Game";
 import NameForm from "./NameForm";
+import { useState } from "react";
+import { Highscore } from "./useHighscore";
 
 function App() {
+  const [player, setPlayer] = useState<Highscore>({
+    id: "",
+    name: "",
+    time: 0,
+  });
+
+  const addPlayerName = (newName: string) => {
+    setPlayer({ ...player, name: newName });
+  };
+
+  const addPlayerTime = (newTime: number) => {
+    setPlayer({ ...player, time: newTime });
+  };
   return (
     <>
-      <NameForm />
-      <Game />
+      <NameForm addPlayer={addPlayerName} />
+      <Game addTime={addPlayerTime} player={player} />
       <Cursor />
     </>
   );
